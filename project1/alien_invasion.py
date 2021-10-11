f75fa35ac04a2d1796ba8846273811c1cbe39778
Montalvas <lucas_montalvani@hotmail.com>
2021-10-11 06:37:35 -0300

    pygame project alien invasion

---
+++ project1/alien_invasion.py
@@ -0,0 +1,27 @@
+import sys, pygame
+from settings import Settings
+
+
+def run_game():
+    """Inicializa o jogo e cria um objeto para a tela"""
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Inicia o laço principal do jogo
+    while True:
+        # Observa eventos de teclado e de mouse
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # Redesenha a tela a cada passagem pelo laço
+        screen.fill(ai_settings.bg_color)
+        
+        # Deixa a tela mais recente visível
+        pygame.display.flip()
+        
+
+
+run_game()
---
+++ project1/settings.py
@@ -0,0 +1,11 @@
+class Settings():
+    """Uma classe para armazenar todas as configurações da Invasão Alienígina.
+    
+    """
+    
+    def __init__(self) -> None:
+        """Inicializa as configurações do jogo."""
+        # Configurações da tela
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
